a6e2dcedf46191ab3029cf6fbeddf36071d22f68
Agnes Leroy <agnes.leroy@zama.ai>
2025-07-02 13:33:21 +0200

    chore(gpu): refactor size tracker to avoid future bugs

--- backends/tfhe-cuda-backend/cuda/src/integer/scalar_div.cuh
+++ backends/tfhe-cuda-backend/cuda/src/integer/scalar_div.cuh
@@ -21,7 +21,7 @@ __host__ uint64_t scratch_integer_unsigned_scalar_div_radix(
       streams, gpu_indexes, gpu_count, params, num_radix_blocks,
       allocate_gpu_memory, is_divisor_power_of_two,
       log2_divisor_exceeds_threshold, multiplier_exceeds_threshold,
-      ilog2_divisor, num_scalar_bits, &size_tracker);
+      ilog2_divisor, num_scalar_bits, size_tracker);
 
   return size_tracker;
 }
